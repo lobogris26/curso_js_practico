1188e200164950cce3dfcf852085afc26c27c98f
lobogris26 <danielmaio62@gmail.com>
2022-02-12 17:16:02 -0300

    primer commit

---
+++ figuras.js
@@ -0,0 +1,15 @@
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden:" + ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es :" + perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es :" + areaCuadrado);
+
+//Código del Triángulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTriangulo3 = 6;
+console.log("Los lados del triángulo miden: " + ladoTriangulo1 + " " + ladoTriangulo2 + " " + ladoTriangulo3)
+
