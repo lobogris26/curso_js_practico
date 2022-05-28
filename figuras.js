cf96a73131bed6a70ccf9d12c38e9c325abe5e54
Daniel <danielmaio62@gmail.com>
2022-05-28 16:13:12 -0300

    termino circulo y creo notas.md

--- figuras.js
+++ figuras.js
@@ -1,15 +1,55 @@
+//Código del cuadrado
+console.group("Cuadrado");
 const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden:" + ladoCuadrado);
+console.log("Los lados del cuadrado miden:" + ladoCuadrado + "cm");
 
 const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perímetro del cuadrado es :" + perimetroCuadrado);
+console.log("El perímetro del cuadrado es :" + perimetroCuadrado + "cm");
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El area del cuadrado es :" + areaCuadrado);
+console.log("El area del cuadrado es :" + areaCuadrado + "cm2");
+console.groupEnd();
 
 //Código del Triángulo
+console.group("Triángulo");
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
-const ladoTriangulo3 = 6;
-console.log("Los lados del triángulo miden: " + ladoTriangulo1 + " " + ladoTriangulo2 + " " + ladoTriangulo3)
+const baseTriangulo3 = 4;
+const alturaTriangulo = 5.5;
 
+console.log("Los lados del triángulo miden: " 
++ ladoTriangulo1 + "cm, " 
++ ladoTriangulo2 + "cm, " 
++ baseTriangulo3 + "cm");
+
+console.log("La altura del triángulo es de " + alturaTriangulo + "cm");
+const perimetroTriangulo = (ladoTriangulo1+ladoTriangulo2+baseTriangulo3);
+console.log("El perímetro del triángulo es :" + perimetroTriangulo + "cm");
+const areaTriangulo = (baseTriangulo3*alturaTriangulo) /2;
+console.log("El area del triángulo es :" + areaTriangulo + "cm2");
+console.groupEnd();
+
+//Código del Círculo
+console.group("Círculo");
+
+// Radio
+const radioCirculo = 4;
+console.log("El radio del círculo es " + radioCirculo + "cm");
+
+// Diámetro
+const diametroCirculo = radioCirculo*2;
+console.log("El diámetro del círculo es " + diametroCirculo + "cm");
+
+// Pi
+const PI = Math.PI;
+console.log("El PI del círculo es " + PI);
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log("El perímetro del círculo es " + perimetroCirculo + "cm");
+
+// Area
+const areaCirculo = (radioCirculo*radioCirculo*PI);
+console.log("El area del círculo es " + areaCirculo + "cm2");
+
+console.groupEnd();
---
+++ notas.md
@@ -0,0 +1,13 @@
+# Notas de las Prácticas de JS
+
+console.log
+console.group
+console.groupEnd
+Math.PI
+>esto es una cita
+- lista
+- segundo articulo de la lista
+1. Lista numerada
+2. segundo art de lista numerada
+___
+
